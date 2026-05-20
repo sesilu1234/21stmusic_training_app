fd121a664d030dc355e3362448ced0bea98e74b3
root <root@SESILU.localdomain>
2026-05-21 00:37:54 +0200

    asdsa

--- app/play/intervalos/diapason/page.tsx
+++ app/play/intervalos/diapason/page.tsx
@@ -152,8 +152,8 @@ export default function IntervalosGame() {
         </div>
 
         {/* QUESTION IMAGE */}
-        <div className="relative flex flex-col items-center w-full max-w-sm md:max-w-md mb-8">
-          <div className="bg-white p-6 md:p-8 rounded-[2.5rem] md:rounded-[3.5rem] shadow-2xl w-full h-40 md:h-48 flex items-center justify-center border-4 border-white relative overflow-hidden">
+        <div className="relative flex flex-col items-center w-full max-w-sm md:max-w-lg mb-8">
+          <div className="bg-white p-2 md:p-2 rounded-[2.5rem] md:rounded-[3.5rem] shadow-2xl w-full h-44 md:h-52 flex items-center justify-center border-4 border-white relative overflow-hidden">
             <div className="absolute top-3 right-5 text-black/10 font-black italic text-lg">
               #{step + 1}
             </div>
